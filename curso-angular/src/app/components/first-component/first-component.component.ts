9506e2cb68d6d118157c7130a3d5c7232f35a1bd
carlosismaelad <carlosismaelad@outlook.com>
2023-10-14 11:29:00 -0300

    Usando dados dinâmicos nos components

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit {
+
+  name: string = 'Carlos'
+  profissao: string = 'Programador'
+  idade: number = 31
+  hobbies = ['Correr', 'Estudar', 'Programar']
+  veiculo = {
+    tipo: 'Moto',
+    modelo: 'Bros',
+    ano: 2022
+  }
+
+  constructor(){}
+
+  ngOnInit(): void {}
 
 }
