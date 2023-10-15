e08fa1548f57f5fff99c41a19bdcbefb90b5045d
carlosismaelad <carlosismaelad@outlook.com>
2023-10-15 14:46:23 -0300

    Introdução ao Angular Router

--- curso-angular/src/app/app-routing.module.ts
+++ curso-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { EmiterComponent } from './components/emiter/emiter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'emiter', component: EmiterComponent},
+  {path: 'pipes', component: PipesComponent},
+  {path: 'list', component: ListRenderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
@@ -31,8 +32,8 @@ import { TwoWayBindingComponent } from './components/two-way-binding/two-way-bin
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
