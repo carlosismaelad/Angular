2cc834a5ce7723ee8984db3e76850f3aa7201dfd
carlosismaelad <carlosismaelad@outlook.com>
2023-10-15 15:58:38 -0300

    Refatorando o service usando o método delete do HTTP

--- curso-angular/src/app/services/list.service.ts
+++ curso-angular/src/app/services/list.service.ts
@@ -14,8 +14,8 @@ export class ListService {
 
   constructor(private http: HttpClient) { }
 
-  remove(animals: Animal[], animal: Animal){
-    return animals.filter((a) => animal.name !== a.name)
+  remove(id: number){
+    return this.http.delete<Animal>(`${this.apiURL}/${id}`)
   }
 
   getAll(): Observable<Animal[]>{
